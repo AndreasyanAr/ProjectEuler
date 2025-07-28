1a2bfa06be2802f2dc8918e30a98ad4afdeddeed
Ara Andreas <ara.andreasyan05@gmail.com>
2025-07-28 14:43:53 -0400

    my com

---
+++ task1.py
@@ -0,0 +1,8 @@
+sum_nums = 0
+for i in range(0,100000001):
+    if i % 3 == 0 or i % 5 == 0:
+        sum_nums += i
+
+print(sum_nums)
+# This code calculates the sum of all numbers from 0 to 10000 that are divisible by 3 or 5.
+# It initializes a variable `sum_nums` to zero, then
